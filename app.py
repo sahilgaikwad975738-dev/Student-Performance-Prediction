4149aff327ef0f49168212f95bb830a6f58ab4e1
Sahil Gaikwad <sahilgaikwad975738@gmail.com>
2026-06-04 14:28:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎓 Student Result")
+st.title("Student Result")
 
 name = st.text_input("Student Name")
 maths = st.number_input("Maths", 0, 100)
@@ -10,9 +10,12 @@ english = st.number_input("English", 0, 100)
 if st.button("Predict"):
     percentage = (maths + science + english) / 3
     grade = "A" if percentage >= 80 else "B" if percentage >= 60 else "C" if percentage >= 40 else "F"
-    result = "✅ PASS" if percentage >= 40 else "❌ FAIL"
 
     st.write(f"**Name:** {name}")
     st.write(f"**Percentage:** {percentage:.2f}%")
     st.write(f"**Grade:** {grade}")
-    st.success(result) if percentage >= 40 else st.error(result)
+
+    if percentage >= 40:
+        st.success("PASS")
+    else:
+        st.error("FAIL")
